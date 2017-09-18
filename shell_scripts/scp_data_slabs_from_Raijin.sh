a253b145f8d1442ddbe739cae897e48c7148840a
Earl Duran <earl.r.duran@gmail.com>
2017-09-18 15:42:33 +1000

    first commit. all sh scripts. about to create tau mean py scripts.

---
+++ shell_scripts/scp_data_slabs_from_Raijin.sh
@@ -0,0 +1,3 @@
+scp -r erd561@raijin.nci.org.au:/short/e14/erd561/PROJECT2/KDS75 /Users/earl/Dropbox/Data/PROJECT2/.
+scp -r erd561@raijin.nci.org.au:/short/e14/erd561/PROJECT2/KDS75_UP /Users/earl/Dropbox/Data/PROJECT2/.
+scp -r erd561@raijin.nci.org.au:/short/e14/erd561/PROJECT2/KDS75_PI /Users/earl/Dropbox/Data/PROJECT2/.
---
+++ shell_scripts/scp_nrca_z_slabs_to_Raijin.sh
@@ -0,0 +1,1 @@
+scp ncra_KDS75_z_slabs.sh erd561@raijin.nci.org.au:/short/e14/erd561/PROJECT2/.
